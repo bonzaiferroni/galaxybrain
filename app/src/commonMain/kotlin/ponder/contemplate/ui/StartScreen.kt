f1a7b6edba4d760808ee35b8bc829f51412bb4cb
Luke Bollwerk <lukas8@gmail.com>
2025-07-31 10:09:55 -0600

    first commit

---
+++ app/src/commonMain/kotlin/ponder/contemplate/ui/StartScreen.kt
@@ -0,0 +1,48 @@
+package ponder.contemplate.ui
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import contemplate.app.generated.resources.Res
+import contemplate.app.generated.resources.compose_multiplatform
+import org.jetbrains.compose.resources.painterResource
+import ponder.contemplate.Greeting
+import ponder.contemplate.HelloRoute
+import pondui.ui.controls.Button
+import pondui.ui.controls.Column
+import pondui.ui.controls.RouteButton
+import pondui.ui.controls.Scaffold
+import pondui.ui.controls.Text
+import pondui.ui.theme.Pond
+
+@Composable
+fun StartScreen() {
+    Scaffold {
+        var showContent by remember { mutableStateOf(false) }
+        Column(
+            spacingUnits = 1,
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            RouteButton("Go to Hello") { HelloRoute }
+            Button(onClick = { showContent = !showContent }) {
+                Text("Show platform")
+            }
+            AnimatedVisibility(showContent) {
+                val greeting = remember { Greeting().greet() }
+                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
+                    Image(painterResource(Res.drawable.compose_multiplatform), null)
+                    Text("Compose: $greeting")
+                }
+            }
+        }
+    }
+}
---
+++ docs/to-galaxy.md
@@ -0,0 +1,8 @@
+git clone https://github.com/bonzaiferroni/contemplate.git galaxy
+cd galaxy
+
+git clone https://github.com/bonzaiferroni/pond-compose.git pondui
+git clone https://github.com/bonzaiferroni/kabinet.git kabinet
+git clone https://github.com/bonzaiferroni/klutch.git klutch
+
+copy libs.versions.toml
